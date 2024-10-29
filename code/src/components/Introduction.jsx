e65d1d5f7f0908427aec2faa1315826ea54e2c18
Daria Vesela <daria1vesela@gmail.com>
2024-10-29 16:15:44 +0000

    layout, colors

--- code/src/components/Introduction.jsx
+++ code/src/components/Introduction.jsx
@@ -7,41 +7,29 @@ const Introduction = (props) => {
       
           {/* Placeholder for Profile Picture */}
           <img src={redProfilePic} alt="Profile" style={{ width: '6cm', height: '6cm', marginRight: '2cm', borderRadius: '50%' }}/>
-        <div>
+        <div style={boxStyle}>
           <p>Hi! My name is Daria!</p>
           <p>I am a ...</p>
         <div style={{ display: 'flex', gap: '10px' }}>
-        <button> Book a Meeting</button>
-        <button> Download Resume</button>
+        <button style={buttonStyle}> Book a Meeting</button>
+        <button style={buttonStyle}> Download Resume</button>
         </div>
         </div>
     </div>
   );
 };
 
-const outerContainerStyle = {
-  display: 'flex',
-  justifyContent: 'center', // Horizontally center the whole container
-  alignItems: 'center', // Vertically center the whole container
-  height: '100vh', // Ensure the container takes up the full height of the viewport
-};
-
-const containerStyle = {
-  display: 'flex', // Aligns items in a row (horizontally)
-  flexDirection: 'row', // Layout the profile picture and box side by side
-  alignItems: 'center', // Vertically center the items inside the container
-  marginBottom: '40px',
-};
 
 const boxStyle = {
   border: '2px solid #BDBDBD', // Light gray border around the box
+  height: '4cm',
   borderRadius: '10px', // Slightly rounded corners
   padding: '20px', // Adds space inside the box
   maxWidth: '300px', // Optional: controls the width of the box
   textAlign: 'center', // Centers the text inside the box
   fontFamily: 'sans-serif', // Clean font family
   boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.2)', // Optional: adds a shadow for depth
-  backgroundColor: 'white', // Sets the background color to white
+  backgroundColor: 'white', // Sets the background color to 
 };
 
 const buttonStyle = {
@@ -55,7 +43,7 @@ const buttonStyle = {
   fontSize: '16px',
   cursor: 'pointer',
   border: 'none',
-  backgroundColor: '#d42417',
+  backgroundColor: '#AB0210ff',
   color: 'white',
   borderRadius: '5px',
 };
