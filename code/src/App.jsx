4bc0b6504c2773684782e02ba1186484352b3cee
Daria Vesela <daria1vesela@gmail.com>
2024-10-30 15:34:12 +0000

    playing with boxes

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -3,9 +3,11 @@ import Introduction from './components/Introduction';
 import Projects from './components/Projects';
 
 function App() {
-  return (<div style={{flex: 1, display: 'flex', flexDirection: 'column', height:'100%'}}>
-    <Introduction style={{flex: 1}}/>
-    <Projects style={{flex: 1}}/>
+  return (<div style={{flex: 1, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh'}}>
+    <div style={{ width: '100%', maxWidth: '800px', display: 'flex',  flexDirection: 'column', gap: 50, marginTop:50 }}>
+    <Introduction style={{flex: 1, width:'100%'}}/>
+    <Projects style={{flex: 1, width: '100%', gap:50, display:'flex', flexDirection: 'column'}}/>
+    </div>
     </div>
   );
 }
